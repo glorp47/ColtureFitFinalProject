836208a71aa10119c8e399987f5695420f9b3648
App Academy Student <student@appacademy.io>
2015-12-15 16:51:31 -0800

    first commit phase 1 underway heroku testing

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,5 +1,5 @@
 class Venue < ActiveRecord::Base
-validates :fan_id, :geo_lat, :geo_lng, :address presence: true
+validates :fan_id, :geo_lat, :geo_lng, :address, presence: true
 
 belongs_to :fan
 has_many :press_items
